f6eedda488085029516c7ed1fd74ad3b09740982
Deepika Sisodia <sisodiadeepika28@gmail.com>
2026-06-06 21:59:19 +0530

    backend completed

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,9 +1,12 @@
 
+import jwt from "jsonwebtoken";
+import User from "../models/User.js";
+
 // middleware to protect routes
-export const protectRoute = async(req, resizeBy, next)=>{
-    try{
+export const protectedRoute = async (req, res, next) => {
+    try {
         const token = req.headers.token;
-        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
         const user = await User.findById(decoded.userId).select("-password");
 
         if(!user) return res.json({success: false, message: "User not found"});
---
+++ server/routes/messageRoutes.js
@@ -0,0 +1,12 @@
+import express from "express";
+import { protectedRoute } from "../middleware/auth.js";
+import { getMessages, getUserForSidebar, markMessageAsSeen, sendMessage } from "../controllers/messageController.js";
+
+const messageRouter = express.Router();
+
+messageRouter.get("/users", protectedRoute, getUserForSidebar);
+messageRouter.get("/:id", protectedRoute, getMessages);
+messageRouter.get("mark/:id", protectedRoute, markMessageAsSeen);
+messageRouter.post("/send/:id", protectedRoute, sendMessage)
+
+export default messageRouter;
